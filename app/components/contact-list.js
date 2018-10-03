fed96472d01ded96bdc08d4c650a6ebd1f12621b
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2018-10-03 16:18:24 +0800

    Updated styling

--- app/components/contact-list.js
+++ app/components/contact-list.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({    
-    
+    actions: {
+        clickToDial(num) {
+            document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
+                type: 'clickToDial',
+                data: { number: num, autoPlace: true }
+            }), "*");
+        }
+      }
 });
