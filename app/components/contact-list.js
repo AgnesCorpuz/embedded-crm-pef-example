286f4af218c1e1461987d41b248b42fcb5b4ea27
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2018-10-03 08:57:18 +0800

    Added toast notification

--- app/components/contact-list.js
+++ app/components/contact-list.js
@@ -1,44 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({    
-    didInsertElement() {
-        window.addEventListener("message", function(event) {
-            var message = JSON.parse(event.data);
-            
-            if(message){
-                if(message.type == "screenPop"){
-                    // document.getElementById("screenPopPayload").value = event.data;
-                } else if(message.type == "contactSearch") {
-                    document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
-                        type: 'sendContactSearch',
-                        data: [{"type": "external", "name": "Weather Line", "phone":[{ "number":"(317) 222-2222", "label":"Cell"}]}]
-                    }), "*");
-
-                    document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
-                        type: 'addTransferContext',
-                        data: {"name": "Case: 1234 - Broken Phone","attributes": {"PEF_TransferContext": "Sample Transfer Context 1234"}}
-                    }), "*");
-                } else if(message.type == "userActionSubscription"){
-                    if(message.data.category == "status") {
-                        this.alert("User Status: " + message.data.data.status); 
-                    } else if(message.data.category == "station") {
-                        
-                    }
-                }
-            }
-        });
-      },
-    selectedStatus: null,
-    actions: {
-        setStatus: function(selected) {
-            this.set("selectedStatus", selected);
-            alert("HI: " + this.get('userName'));
-        },
-        submit: function(){
-            document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
-                type: 'updateUserStatus',
-                data: { id: this.get('selectedStatus') }
-            }), "*");
-        }
-    }
+    
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,19 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+    selectedStatus: null,
     actions: {
         openModal: function(target) {
             var modal = this.get('comp-' + target);
             modal.send('toggleModal');
+        },
+        setStatus: function(selected) {
+            this.set("selectedStatus", selected);
+            document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
+                type: 'updateUserStatus',
+                data: { id: this.get('selectedStatus') }
+            }), "*");
         }
     },
 
@@ -22,5 +31,31 @@ export default Controller.extend({
         `&userLanguage=${this.frameworkConfig.userLanguage}`;
     },
 
-    frameworkConfig: service('framework-config')
+    frameworkConfig: service('framework-config'),
+    
+    addListeners: function() {
+        window.addEventListener("message", function(event) {
+            var message = JSON.parse(event.data);
+            
+            if(message){
+                if(message.type == "screenPop"){
+                    // document.getElementById("screenPopPayload").value = event.data;
+                } else if(message.type == "contactSearch") {
+                    document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
+                        type: 'sendContactSearch',
+                        data: [{"type": "external", "name": "Weather Line", "phone":[{ "number":"(317) 222-2222", "label":"Cell"}]}]
+                    }), "*");
+
+                    document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
+                        type: 'addTransferContext',
+                        data: {"name": "Case: 1234 - Broken Phone","attributes": {"PEF_TransferContext": "Sample Transfer Context 1234"}}
+                    }), "*");
+                } else if(message.type == "userActionSubscription"){
+                    if(message.data.category == "status") {
+                        toastr.info("User Status: " + message.data.data.status);
+                    }
+                }
+            }
+        });
+    }.on('init')
 });
