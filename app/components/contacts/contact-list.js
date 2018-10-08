7ee76439414d476c634b8ab6ef4e8c16050bf681
hero2bash <zero.bash@gmail.com>
2018-10-08 15:43:45 +0800

    Add contact details + component clicktodial

--- app/components/contacts/contact-list.js
+++ app/components/contacts/contact-list.js
@@ -17,11 +17,6 @@ export default Component.extend({
         }
     },
     actions: {
-        clickToDial(num) {
-            document.getElementById("softphone").contentWindow.postMessage(JSON.stringify({
-                type: 'clickToDial',
-                data: { number: num, autoPlace: true }
-            }), "*");
-        }
+        
     }
 });
